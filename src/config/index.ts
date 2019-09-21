ee54e5ae1ae3960e5bea590543d3c4756739a609
by1773 <1747837358@qq.com>
2019-09-21 09:38:01 +0800

    -

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,16 +4,19 @@
  * @Author: by1773
  * @Date: 2019-09-17 13:54:16
  * @LastEditors: by1773
- * @LastEditTime: 2019-09-20 16:33:19
+ * @LastEditTime: 2019-09-20 22:37:53
  */
 // export const ONLINEHOST = 'https://api.apiopen.top';
 // http://172.16.2.9:8081/swagger-ui.html
-export const ONLINEHOST = 'http://172.16.2.9:8081/';
+// export const ONLINEHOST = 'http://172.16.2.9:8081/'; 
+export const ONLINEHOST = 'http://139.9.1.139:8083/'; 
+/** 
 /** 
  * mock 接口p
  * */ 
 // export const MOCKHOST = 'https://api.apiopen.top';
-export const MOCKHOST = 'http://172.16.2.9:8081/';
+// export const MOCKHOST = 'http://172.16.2.9:8081/';
+export const MOCKHOST = 'http://139.9.1.139:8083/'; 
 
 /** 
  * 是否mock
@@ -34,4 +37,4 @@ export const SHAREINFO = {
     'title': '分享标题',
     'path': '路径',
     'imageUrl': '图片'
-  }
+  } 
